685a1d3abd0664d71555f5bf82a2dc8e1e0239ff
齋藤創 <b1011177@hotmail.co.jp>
2018-06-02 00:43:20 +0900

    firebase DB連携

--- src/pages/HouseholdAccountBook.vue
+++ src/pages/HouseholdAccountBook.vue
@@ -2,20 +2,45 @@
   <div class="HouseHold">
     <h1>カケイボ</h1>
     <VoiceInput></VoiceInput>
+    <pre>{{ costData }}</pre>
   </div>
 </template>
 
 <script>
 import VoiceInput from '@/components/Voice_input'
+import firebase from 'firebase'
+import firebaseConf from '../.firebaseEnv.json'
+
 export default {
   name: 'HouseHold',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
+  data: () => ({
+    fireDB: '',
+    costDB: '',
+    costData: []
+  }),
   components: {
     VoiceInput
+  },
+  methods: {
+    syncFirebase () {
+      this.costDB = this.fireDB.database().ref('costs')
+      let list = []
+      this.costDB.on('value', function (fbdata) {
+        console.log('fvdata', fbdata.val())
+        list.push(fbdata.val())
+      })
+      this.costData = list
+    }
+  },
+  created () {
+    this.fireDB = !firebase.app.length ? firebase.initializeApp(firebaseConf) : firebase.app()
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.syncFirebase()
+      } else {
+        alert('ログインしてね♪')
+      }
+    })
   }
 }
 </script>
