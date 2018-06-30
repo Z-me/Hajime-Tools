901a153db4d36fdac2dac9303d9f227246fd044d
齋藤創 <b1011177@hotmail.co.jp>
2018-06-30 14:10:17 +0900

    各種データをグラフ化

--- src/components/table.vue
+++ src/components/table.vue
@@ -42,30 +42,31 @@
       </md-table-row>
     </md-table>
     <div class="md-layout md-gutter md-alignment-center result-field" v-if="chartData != null">
-      <div class="md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100">
-        <p>全体支払割合</p>
+      <div class="md-layout-item md-xlarge-size-50 md-large-size-50 md-medium-size-50 md-small-size-50 md-xsmall-size-100">
+        <h3 class="center">全体支払割合</h3>
         <doughnut-chart :width="210" :height="70" :chart-data="allGenreDonuts"></doughnut-chart>
+        <h1 class="center">¥ {{totalCost}}</h1>
       </div>
-      <div class="md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100">
-        <p>支払い者割合</p>
+      <div class="md-layout-item md-xlarge-size-50 md-large-size-50 md-medium-size-50 md-small-size-50 md-xsmall-size-100">
+        <h3 class="center">支払い者割合</h3>
         <holizontal-bar :width="210" :height="70" :chart-data="paymentRate"></holizontal-bar>
       </div>
 
-      <div class="md-layout-item md-medium-size-100 md-small-size-100 md-xsmall-size-100">
-        <p>支出日グラフ</p>
-        <line-chart :width="420" :height="70" :chart-data="chartData"></line-chart>
+      <div class="md-layout-item md-xlarge-size-100 md-large-size-100 md-medium-size-100 md-small-size-100 md-xsmall-size-100">
+        <h3 class="center">支出日グラフ</h3>
+        <line-chart :width="420" :height="70" :chart-data="dailyCosts"></line-chart>
       </div>
-      <div class="md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100">
-        <p>支出者別支払割合(Hajime)</p>
-        <pie-chart :width="210" :height="100" :chart-data="chartData"></pie-chart>
+      <div class="md-layout-item md-xlarge-size-50 md-large-size-50 md-medium-size-50 md-small-size-50 md-xsmall-size-100">
+        <h3 class="center">支出者別支払割合(Hajime)</h3>
+        <doughnut-chart :width="210" :height="100" :chart-data="getPaymentCosts('Hajime')"></doughnut-chart>
+        <h1 class="center">¥ {{totalCosts.Hajime}}</h1>
       </div>
-      <div class="md-layout-item md-medium-size-50 md-small-size-50 md-xsmall-size-100">
-        <p>支出者別支払割合(Shiori)</p>
-        <pie-chart :width="210" :height="100" :chart-data="chartData"></pie-chart>
+      <div class="md-layout-item md-xlarge-size-50 md-large-size-50 md-medium-size-50 md-small-size-50 md-xsmall-size-100">
+        <h3 class="center">支出者別支払割合(Shiori)</h3>
+        <doughnut-chart :width="210" :height="100" :chart-data="getPaymentCosts('Shiori')"></doughnut-chart>
+        <h1 class="center">¥ {{totalCosts.Shiori}}</h1>
       </div>
     </div>
-    <pre>{{chartData}}</pre>
-    <button @click="fillData()">Randomize</button>
   </div>
 </template>
 
@@ -112,7 +113,10 @@ export default {
     selectedDay: 'All Data in List',
     chartData: null,
     allGenreDonuts: null,
-    paymentRate: null
+    paymentRate: null,
+    dailyCosts: null,
+    totalCost: 0,
+    totalCosts: {}
   }),
   props: [
     'items'
@@ -165,7 +169,73 @@ export default {
         }
       }
       this.dayObj = obj
-      console.log(obj)
+    },
+    sortFunc (arr) {
+      arr.sort(function (a, b) {
+        if (a < b) return -1
+        if (a > b) return 1
+        return 0
+      })
+      return arr
+    },
+    validDate (y, m, d) {
+      let dt = new Date(y, m - 1, d)
+      return (dt.getFullYear() === y && dt.getMonth() === m - 1 && dt.getDate() === d)
+    },
+    getAllDay (startDay, endDay) {
+      let result = []
+      let start = startDay.slice(0, 7) + '-01'
+      let endDate = [
+        endDay.slice(0, 4),
+        endDay.slice(5, 7)
+      ]
+      let end = null
+      if (this.validDate(Number(endDate[0]), Number(endDate[1]), 31)) {
+        end = endDay.slice(0, 7) + '-31'
+      } else if (this.validDate(Number(endDate[0]), Number(endDate[1]), 30)) {
+        end = endDay.slice(0, 7) + '-30'
+      } else if (this.validDate(Number(endDate[0]), Number(endDate[1]), 29)) {
+        end = endDay.slice(0, 7) + '-29'
+      } else {
+        end = endDay.slice(0, 7) + '-28'
+      }
+      let inputObj = {
+        year: Number(start.slice(0, 4)),
+        month: Number(start.slice(5, 7)),
+        day: Number(start.slice(8, 10))
+      }
+      let endObj = {
+        year: Number(end.slice(0, 4)),
+        month: Number(end.slice(5, 7)),
+        day: Number(end.slice(8, 10))
+      }
+      let dateFlag = true
+      while (dateFlag) {
+        if (this.validDate(inputObj.year, inputObj.month, inputObj.day)) {
+          result.push(this.getDateString(inputObj.year, inputObj.month, inputObj.day))
+          inputObj.day++
+        } else {
+          if (this.validDate(inputObj.year, inputObj.month + 1, 1)) {
+            result.push(this.getDateString(inputObj.year, inputObj.month + 1, 1))
+            inputObj.month++
+            inputObj.day = 2
+          } else if (this.validDate(inputObj.year + 1, 1, 1)) {
+            result.push(this.getDateString(inputObj.year + 1, 1, 1))
+            inputObj.year++
+            inputObj.month = 1
+            inputObj.day = 2
+          }
+        }
+        let input = new Date(inputObj.year, inputObj.month, inputObj.day)
+        let end = new Date(endObj.year, endObj.month, endObj.day)
+        if (input > end) {
+          dateFlag = false
+        }
+      }
+      return result
+    },
+    getDateString (year, month, day) {
+      return year + '-' + (month < 10 ? '0' + month : month) + '-' + (day < 10 ? '0' + day : day)
     },
     createCalcResult () {
       return null
@@ -188,12 +258,15 @@ export default {
       }
       this.setAllGenreDonuts()
       this.setPaymentRate()
+      this.setDailyCosts()
     },
     setAllGenreDonuts () {
       let labels = []
       let datas = {}
+      let costs = 0
       let data = Object.assign({}, this.searched)
       for (let key in data) {
+        costs += Number(data[key].cost)
         if (labels.indexOf(String(data[key].type)) < 0) {
           labels.push(data[key].type)
           datas[data[key].type] = Number(data[key].cost)
@@ -214,6 +287,7 @@ export default {
         }]
       }
       console.log('allGenre', this.allGenreDonuts)
+      this.totalCost = costs
       return datas
     },
     setPaymentRate () {
@@ -242,6 +316,98 @@ export default {
 
       console.log('paymentRate', this.paymentRate)
       return datas
+    },
+    setDailyCosts () {
+      let data = Object.assign({}, this.searched)
+      let types = ['ALL']
+      let days = []
+      let datas = {
+        'ALL': []
+      }
+      Object.keys(data).forEach(function (k) {
+        days.push(data[k].date)
+      })
+      let dayRange = days.filter(function (x, i, self) {
+        return self.indexOf(x) === i
+      })
+      dayRange = Object.assign([], this.sortFunc(dayRange))
+      let startDate = dayRange[0]
+      let endDate = dayRange[dayRange.length - 1]
+      dayRange = Object.assign([], this.getAllDay(startDate, endDate))
+      dayRange = Object.assign([], this.sortFunc(dayRange))
+      for (let key in data) {
+        if (types.indexOf(String(data[key].type)) < 0) {
+          types.push(data[key].type)
+          datas[data[key].type] = []
+        }
+      }
+      for (let i = 0; i < dayRange.length; i++) {
+        for (let j = 0; j < types.length; j++) {
+          let cost = 0
+          for (let key in data) {
+            if (dayRange[i] === data[key].date && types[j] === data[key].type) {
+              cost += Number(data[key].cost)
+            }
+          }
+          datas[types[j]].push(cost)
+        }
+      }
+      let all = []
+      for (let i = 0; i < dayRange.length; i++) {
+        let allCost = 0
+        for (let j = 0; j < types.length; j++) {
+          allCost += datas[types[j]][i]
+        }
+        all.push(allCost)
+      }
+      datas.ALL = Object.assign([], all)
+      let datasets = []
+      for (let i in types) {
+        let dataset = {
+          label: types[i],
+          backgroundColor: colorList[i],
+          data: datas[types[i]]
+        }
+        datasets.push(dataset)
+      }
+      this.dailyCosts = {
+        labels: dayRange,
+        datasets: datasets
+      }
+
+      console.log('dailyCosts', this.dailyCosts)
+      return datasets
+    },
+    getPaymentCosts (payment) {
+      let data = Object.assign({}, this.searched)
+      let types = []
+      let datas = []
+      let costs = 0
+      for (let key in data) {
+        if (String(data[key].payment) === payment) {
+          costs += Number(data[key].cost)
+          if (types.indexOf(String(data[key].type)) < 0) {
+            types.push(data[key].type)
+            datas[data[key].type] = Number(data[key].cost)
+          } else {
+            datas[data[key].type] += Number(data[key].cost)
+          }
+        }
+      }
+      console.log('tyeps:_', types)
+      console.log('datas:_', datas)
+      let result = {
+        labels: types,
+        datasets: [
+          {
+            label: payment + ' 支払い割合',
+            backgroundColor: colorList,
+            data: Object.values(datas)
+          }
+        ]
+      }
+      this.totalCosts[payment] = costs
+      return result
     }
   },
   created () {
@@ -275,5 +441,8 @@ export default {
 }
 .table-header-button {
   color: #FFF !important;
+}
+.center {
+  text-align: center !important;
 }
 </style>
